4518f0f2fa568b0623816f54623067841c3a58c4
Yun-Ho SONG <imsong1997@gmail.com>
2024-03-11 22:19:28 +0900

    Firestore 로직 추가

--- data/src/main/java/com/tnt/attendance_data/remote/Firestore.kt
+++ data/src/main/java/com/tnt/attendance_data/remote/Firestore.kt
@@ -2,6 +2,7 @@ package com.tnt.attendance_data.remote
 
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import io.reactivex.Single
 
 
@@ -19,7 +20,7 @@ class Firestore {
             }
     }
 
-    fun getMonthList(year: String): Single<ArrayList<String>> =
+    fun getMonthList(year: String, nowMonth: String): Single<ArrayList<String>> =
         Single.create { emitter ->
             val monthList = arrayListOf<String>()
             FirebaseFirestore.getInstance().collection(year).get()
@@ -33,12 +34,20 @@ class Firestore {
                     Log.e("Firestore - getMonthList", it.printStackTrace().toString())
                     emitter.onError(Throwable("Firestore - getMonthList"))
                 }
+        }.map {
+            // 새로운 월 바뀔 때 doc이 없을 시 자동 추가
+            if (!it.contains(nowMonth)) {
+                setNewDocument(year, nowMonth)
+                it.add(nowMonth)
+            }
+            it
         }
 
     fun getAttendMemberList(year: String, month: String): Single<String> =
         Single.create { emitter ->
             FirebaseFirestore.getInstance().collection(year).document(month).get()
                 .addOnCompleteListener {
+                    println(it.result.data?.toString())
                     emitter.onSuccess(it.result.data?.toString() ?: "")
                 }
                 .addOnFailureListener {
@@ -46,4 +55,33 @@ class Firestore {
                     emitter.onError(Throwable("Firestore - getAttendMemberList"))
                 }
         }
+
+    fun setAttendList(year: String, month: String, attendMap: Map<String, ArrayList<String>>): Single<Boolean> =
+        Single.create { emitter ->
+            FirebaseFirestore.getInstance()
+                .collection(year).document(month)
+                .set(attendMap, SetOptions.merge())
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        emitter.onSuccess(true)
+                    } else {
+                        emitter.onSuccess(false)
+                    }
+                }
+                .addOnFailureListener {
+                    Log.e("Firestore - setAttendList", it.printStackTrace().toString())
+                    emitter.onSuccess(false)
+                }
+        }
+
+    private fun setNewDocument(year: String, month: String) {
+        FirebaseFirestore.getInstance().collection(year).document(month).set(hashMapOf<Int, ArrayList<String>>())
+            .addOnSuccessListener {
+                println("add New Document success")
+            }.addOnFailureListener {
+                it.printStackTrace()
+                println("add New Document fail")
+
+            }
+    }
 }
